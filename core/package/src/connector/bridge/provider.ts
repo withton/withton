1a382f14fb79b971a634be3b26517fff274b9c2d
itsalimanuel <itskhaloufali@gmail.com>
2024-09-08 18:41:12 +0400

    chore: package

--- core/package/src/connector/bridge/provider.ts
+++ core/package/src/connector/bridge/provider.ts
@@ -24,3 +24,25 @@ import {
   isPendingHttpConnection,
 } from "../../connector/bridge/core/connection";
 
+import { BridgeIMessage } from "./core/IMessage";
+import { BridgeSession, BridgeSessionType } from "./core/session";
+import { ConnectionManager } from "../../storage/connect";
+import { StorageType } from "../../storage/models/types";
+import {
+  MakeOptional,
+  RemoveId,
+  DistributiveRemoveId,
+} from "../../helpers/types";
+
+import { BRIDGE_VERSION } from "../../config/bridge";
+import { loggerDebug, loggerError } from "../../helpers/logger";
+import {
+  isTelegramLink,
+  encodeTelegramParams,
+} from "../../helpers/tg-url-formater";
+import { onSuccess } from "../../helpers/onSuccess";
+import { createAbortManager } from "../../helpers/abortManager";
+
+
+
+export class ConnectorProvider implements HttpConnection {}
